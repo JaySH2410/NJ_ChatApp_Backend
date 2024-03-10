50c5cb1887808db10d623677bb1b321ad9a69674
jaysh <jsheth066@gmail.com>
2024-03-10 20:45:33 +0530

    Initial Commit

---
+++ app.js
@@ -0,0 +1,18 @@
+// Import required modules
+const express = require('express');
+const http = require("http");
+const SocketServices = require("./socket");
+
+// Create an Express application
+const app = express();
+const server = http.createServer(app);
+const socketServices = new SocketServices();
+
+socketServices.io.attach(server);
+
+const PORT = process.env.PORT || 8000;
+server.listen(PORT, () => {
+  console.log(`Http server started at port:${PORT}`);
+});
+
+socketServices.initListener();
---
+++ socket.js
@@ -0,0 +1,50 @@
+const { Server } = require('socket.io');
+const { Redis } = require("ioredis");
+
+// const PORT = process.env.REACT_APP_REDIS_PORT
+
+const pub = new Redis({
+ 
+});
+const sub = new Redis({
+ 
+});
+class SocketServices {
+    constructor() {
+      console.log("Init Socket Server");
+      this._io = new Server({
+        cors: {
+          allowedHeaders: ["*"],
+          origin: "*",
+        },
+      });
+  
+      sub.subscribe("MESSAGE");
+    }
+  
+    initListener() {
+      console.log("Init Socket listeners");
+      const io = this.io;
+      io.on("connect", (socket) => {
+        console.log("New Socket connected", socket.id);
+        socket.on("event:message", async ({ message }) => {
+          console.log("New Message is sent", message);
+          //pub this msg to redis
+          await pub.publish("MESSAGE", JSON.stringify({ message }));
+        });
+      });
+      sub.on("message", async (channel, message) => {
+        if (channel === "MESSAGE") {
+          console.log("Message from Redis:", message);
+  
+          io.emit("message", message);
+        }
+      });
+    }
+  
+    get io() {
+      return this._io;
+    }
+  }
+  
+  module.exports = SocketServices;
